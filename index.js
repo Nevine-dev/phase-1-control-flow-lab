a11833782153a72e6fd639625bb0ecfeb2d492fe
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-03-17 08:49:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (dist <= 400 ){
+    return "This one is on me!"
+  }
+  else if (dist > 2000 && dist <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (dist > 2500) {
+    return "No can do."
+  }
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  return city === 'NYC'? 'Ok, sounds good.':"No go."
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+
+    default :
+      return 'Bye.'
+
+
+
+  }
 }
